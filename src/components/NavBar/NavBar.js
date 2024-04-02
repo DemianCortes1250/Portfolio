1a0b1f7904cec66b3aab50a922ecb5f0d7de6a33
DemianCortes1250 <demiancmusic@gmail.com>
2024-04-02 02:48:27 -0300

    nav

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom';
 const NavBar = () => {
 
     return (
-<div className="flex justify-end items-center border-b-2 border-outset border-Color2 h-18">
-    <div className="font-mono space-x-8 text-Color1 md:text-2xl p-1 md:p-2 m-1 md:m-2 pr-2">
-                <Link to="/" className="About">About</Link>
-                <Link to="/Skills" className="Skills">Skills</Link>
-                <Link to="/Proyects" className="Proyects">Proyects</Link>
-                <Link to="/Contact" className="Contact">Contact</Link>
+        <div className="flex justify-end items-center border-b-2 border-Color2 h-20">
+            <div className="font-mono space-x-8 text-Color1 lg:text-2xl md:m-2 pr-2">
+                <Link to="/">About</Link>
+                <Link to="/Skills">Skills</Link>
+                <Link to="/Proyects">Proyects</Link>
+                <Link to="/Contact">Contact</Link>
             </div>
         </div>
     )
